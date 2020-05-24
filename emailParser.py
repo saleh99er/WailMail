d16d0c14b0b90de3e85e8baa5fde89ca1b324df3
Saleh Hassen <saleh99er@gmail.com>
2020-05-23 22:51:35 -0400

    some progress with emailParser, need to account for substrings, can't simply use replace for expression string

---
+++ emailParser.py
@@ -0,0 +1,54 @@
+import time
+import re
+
+class Rule:
+    def __init__(self, id, condition, audio):
+        self.id = id
+        self.condition = condition
+        self.audio = audio
+    
+class Condition:
+    do_not_replace = ['not', 'and', 'or', '(', ')']
+
+    def neq(s, check_these):
+        for check in check_these:
+            if(s == check):
+                return False
+        return True
+
+    def adjust_paren(str_list):
+        i = 0
+        while(i < len(str_list)):
+            s = str_list[i]
+            open_paren_index = s.find("(")
+            close_paren_index = s.find(")")
+            if(open_paren_index > -1):
+                term = s[open_paren_index+1:]
+                str_list[i] = "("
+                str_list.insert(i+1, term)
+            elif(close_paren_index > 0):
+                term = s[:close_paren_index]
+                str_list[i] = term
+                str_list.insert(i+1, ")")
+            i += 1
+        
+    def __init__(self, cond_str):
+        self.terms = {} # string key, boolean val dictionary
+        self.str_list = cond_str.split()
+        Condition.adjust_paren(self.str_list)
+        self.cond_str = " ".join(str_list)
+        for word in self.str_list:
+            if(Condition.neq(word, Condition.do_not_replace)):
+                self.terms[word] = False
+
+    def setTrue(self, word):
+        if(word in self.terms):
+            self.terms[word] = True
+
+    def eval(self):
+        temp = self.cond_str.split("*")
+         
+        for key in self.terms.keys():
+            eval_str = eval_str.replace(key,str(self.terms[key]))
+        return eval(eval_str)
+
---
+++ emailParser_test.py
@@ -0,0 +1,42 @@
+from emailParser import *
+
+def condition_tests():
+    condition_grades = Condition("(cms or gradescope) or piazza or cornell.edu")
+    result = condition_grades.eval()
+    assert(result == False)
+    
+    condition_grades.setTrue("fake")
+    result = condition_grades.eval()
+    assert(result == False)
+
+    condition_grades.setTrue("gradescope")
+    result = condition_grades.eval()
+    assert(result == True)
+
+    condition_grades.setTrue("cornell.edu")
+    result = condition_grades.eval()
+    assert(result == True)
+    
+    condition_workday = Condition("workday and not (timestamp and due)")
+    result = condition_workday.eval()
+    assert(result == False)
+
+    condition_workday.setTrue("workday")
+    result = condition_workday.eval()
+    assert(result == True)
+
+    condition_workday.setTrue("timestamp")
+    result = condition_workday.eval()
+    assert(result == True)
+
+    condition_workday.setTrue("due")
+    result = condition_workday.eval()
+    assert(result == False)
+
+    condition_campus = Condition("not north and not northcampus")
+    result = condition_campus.eval()
+    assert(result == True)
+
+    print("Condition class tests passed")
+
+condition_tests()
